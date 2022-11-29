8e3145046a3098a82986f356dd1e48933f2c04fe
thanhduong <xuanthanhduong94@gmail.com>
2022-11-29 19:51:29 +0100

    branch: smartphone exercise

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,13 +1,32 @@
 package de.neuefische;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Citizen bioStudent1=new BiologyStudent();
-        Citizen computerStudent1=new ComputerScienceStudent();
-        Citizen[] students={bioStudent1,computerStudent1};
-        System.out.println(students[1].getAddress());
-        System.out.println(computerStudent1.getIdentityCardNumber());
+        Contact friend1 = new Friend("Tobi","0123456");
+        System.out.println(friend1);
 
+        Contact college1 = new BusinessContact("Bob","4562354","Apple");
+        System.out.println(college1);
+
+        Smartphone contactList=new Smartphone(new Contact[]{new Friend("Tobi","0123456"),
+                new Friend("Max","0157334"),
+                new BusinessContact("Bob","4562354","Apple")});
+        //Add contact
+        contactList.addContact(new Friend("Bob","0245677"));
+        System.out.println(contactList);
+
+        //Get contact
+        System.out.println(contactList.getContact(2).toString());
+
+        //Get contact by name
+        System.out.println(Arrays.toString(contactList.getContactByName("Bob")));
+
+        //Remove contact by name
+        System.out.println(Arrays.toString(contactList.removeContactByName("Max")));
 
     }
+
+
 }
